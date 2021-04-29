506ee64087bd734b5e271f3cf36916864b41df0a
Jacques <jacques.adriaan.venter@gmail.com>
2021-04-29 12:35:25 +0200

    Even Queue System
    This is the first version of the event queue system that is ready to be expanded upon.

---
+++ Events/SampleEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Event that informs subscribers of something
+public class SampleEvent : EventData
+{
+    public readonly bool sampleBool;
+
+    public SampleEvent(bool givenBool) : base(EventType.SampleEvent)
+    {
+        sampleBool = givenBool;
+    }
+}
