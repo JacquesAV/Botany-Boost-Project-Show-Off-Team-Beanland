56bfac41abd0a6d887cc4dc8a1117691d8038b46
Ryan <ryan.stander@outlook.com>
2021-05-13 09:39:56 +0200

    Adding basic event manager

---
+++ Events/EventDataInheritances.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Event that informs subscribers of something
+public class SendDebugLog : EventData
+{
+    public readonly string debuglog;
+
+    public SendDebugLog(string givenLog) : base(EventType.RECREIVEDEBUG)
+    {
+        debuglog = givenLog;
+    }
+}
