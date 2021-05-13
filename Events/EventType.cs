1099ffb24d196910b49afde21c76cd78327bbe94
Jacques <jacques.adriaan.venter@gmail.com>
2021-05-13 09:37:47 +0200

    Cleared grid terrain of old files
    Improved separation of branches by avoiding duplicate files from existing in different places.

--- Events/EventType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Defines the different event types to be used in event data in enumeration form
-public enum EventType 
-{
-    DEBUGGERSTARTEVENT=0,
-}
