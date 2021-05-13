8d28507526bd594ad50ed4d5e2cca734e6a5a460
Ryan <ryan.stander@outlook.com>
2021-05-13 12:48:58 +0200

    placeable data script proto version
    Requires addition of stats, but need to talk with designers about what each plant would have

---
+++ Placeables/PlaceableData.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class PlaceableData : MonoBehaviour
+{
+    [Header("Visuals")]
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Sprite displayImage = null;
+    [Header("Data")]
+    [SerializeField] private PlaceableType placeableType;
+    private PlaceableOrientation placeableOrientation;
+    [SerializeField] private Vector2 placeableDimensions = Vector2.zero;
+    [SerializeField] private int cost = 0;
+
+    //The different kinds of placeables that are present in the game
+    private enum PlaceableType
+    {
+        Flower,
+        Bush,
+        Tree,
+        Flooring,
+        Ornaments
+    }  
+
+    private struct PlaceableStats
+    {
+        //Stats need to be added with designers help
+    }
+
+    #region Getters
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public string GetPlaceableType()
+    {
+        return placeableType.ToString();
+    }
+
+    public GameObject GetPrefab()
+    {
+        return prefab;
+    }
+
+    public Sprite GetDisplayImage()
+    {
+        return displayImage;
+    }
+
+    public Vector2 GetDimensions()
+    {
+        return placeableDimensions;
+    }
+
+    public string GetOrientation()
+    {
+        return placeableOrientation.ToString();
+    }
+    #endregion
+
+}
+//Orientation of the placeables
+public enum PlaceableOrientation
+{
+    Left,
+    Right,
+    Forward,
+    Back
+}
