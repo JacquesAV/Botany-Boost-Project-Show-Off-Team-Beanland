de02e35693b367f655b0b9bdda48f838fad544a3
Ryan <ryan.stander@outlook.com>
2021-05-18 15:12:38 +0200

    Added new event types

--- Events/EventDataInheritances.cs
+++ Events/EventDataInheritances.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Event that informs subscribers of something
+//Event that informs subscribers of a debug log
 public class SendDebugLog : EventData
 {
     public readonly string debuglog;
@@ -10,5 +10,16 @@ public class SendDebugLog : EventData
     public SendDebugLog(string givenLog) : base(EventType.RECREIVEDEBUG)
     {
         debuglog = givenLog;
+    }
+}
+
+//Event that informs subscribers of a placeable being selected
+public class PlaceableSelectedOnGUI : EventData
+{
+    public readonly PlaceableData placeable;
+
+    public PlaceableSelectedOnGUI(PlaceableData givenPlaceable) : base(EventType.CLICKEDPLACEABLEGUI)
+    {
+        placeable = givenPlaceable;
     }
 }
--- Events/EventType.cs
+++ Events/EventType.cs
@@ -6,4 +6,5 @@ using UnityEngine;
 public enum EventType 
 {
     RECREIVEDEBUG=0,
+    CLICKEDPLACEABLEGUI
 }
