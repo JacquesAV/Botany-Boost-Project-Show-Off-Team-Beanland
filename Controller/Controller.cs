1e17c6af435d970248b6485481bd165b9bc8a152
Ryan <stander.ryan@gmail.com>
2021-05-26 09:30:46 +0200

    Controller Cleanup
    Cleaned the script so that it was clear and removed old commented out lines

--- Controller/Controller.cs
+++ Controller/Controller.cs
@@ -5,8 +5,6 @@ using UnityEngine.EventSystems;
 
 public class Controller : MonoBehaviour, IPointerClickHandler
 {
-    //private PlaceableData placeableToSelect;//The item that will be selected the next time HandleInput is called
-
     //------------------------------------------------------------------------------------------------------------------------
     //                                                  OnPointerClick()
     //------------------------------------------------------------------------------------------------------------------------ 
@@ -16,14 +14,12 @@ public class Controller : MonoBehaviour, IPointerClickHandler
         //Check if the mouse clicked on an placeable container. 
         IPlaceableContainer placeableContainer = eventData.pointerCurrentRaycast.gameObject.GetComponent<IPlaceableContainer>();
 
-        //If the game object that was clicked on has a component which implements the IItemContainer interface, PlaceableContainer
+        //If the game object that was clicked on has a component which implements the IPlaceableContainer interface, PlaceableContainer
         //will be assigned with that component, otherwise PlaceableContainer would be null, meaning the mouse didn't click on any
         //item containers in the view. If you are clicking on the item container but it still returns null, check to see if there
         //is any other UI elements blocking the raycast, if so mark their 'Raycast Target' property as false in Unity Editor
         if (placeableContainer != null)
         {
-            //placeableToSelect = placeableContainer.Placeable;//Use IPlaceableContainer's getter to get the item in the container, assign it to placeableToSelect,
-                                                             //now it will be selected the next time HandleInput is called
             EventManager.currentManager.AddEvent(new PlaceableSelectedOnGUI(placeableContainer.Placeable));
             //DebugManager.DebugLog("Clicked placeable");
         }
