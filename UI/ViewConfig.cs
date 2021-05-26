2f67bc9ebd895918c45b89c4c93834fe6d43521f
Ryan <stander.ryan@gmail.com>
2021-05-26 10:03:28 +0200

    UI Cleanup
    Cleaned IPlaceableContainer and ViewConfig to be short and explain whats needed

--- UI/ViewConfig.cs
+++ UI/ViewConfig.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// This scriptable object saves global configuration for views, to use it in a similar way as a singleton, create ViewConfig object and put it in Resources
-/// folder, then classes can use Resources.Load to access the needed values, see examples of this usage in the GridViewKeyboardController and ShopGridBuyView
-/// classes
+/// folder, then classes can use Resources.Load to access the needed values
 /// </summary>
 [CreateAssetMenu(fileName ="Data",menuName ="ScriptableObjects/ViewConfig",order =1)]//Allows creating ViewConfig objects in Assets -> Create menu in the Unity Editor
 public class ViewConfig : ScriptableObject
