e8934b00273e6d0826279f496776e4a112021002
Jacques <jacques.adriaan.venter@gmail.com>
2021-06-03 16:28:52 +0200

    Light Intensity Update
    Updated and cleaned time cycles to instead use Lux Intensity for the day and night cycle instead of changing the lights colors.

--- TimeCycles/TimeCycle.cs
+++ TimeCycles/TimeCycle.cs
@@ -7,11 +7,12 @@ public class TimeCycle : MonoBehaviour
 {
     [SerializeField] private float dayLength = 15;//how long day time is
     [SerializeField] private float nightLength = 5;//how long night time is
-
-    [SerializeField] private Color dayColor = new Color(1, 1, 1);//the global lights color during day
-    [SerializeField] private Color nightColor = new Color(0.2196078f, 0.08627451f, 0.08627451f);//the global lights color during night
-
     [SerializeField] private HDAdditionalLightData sceneLight;//The scene light
+
+    private float upperLuxBound = 130000; //The maximum value for the lux emission intensity lighting
+    private float middleLuxBound = 10000; //The middle bound that separates day and night
+    private float lowerLuxBound = 0; //The minimum value for the lux emission intensity lighting
+
     public bool Daytime { get; private set; }//whether it is daytime or nightime
     private float timeRemaining;//amount of time of the current part of day that is left
     private int currentDayInTheWeek=1;//The day of the week
@@ -28,62 +29,104 @@ public class TimeCycle : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Checks for a day change
         AlternateDaytime();
-        ChangeColorGradientBasedOnTime();
+
+        //ChangeColorGradientBasedOnTime();
+        ChangeLightingEmmisionIntensityBasedOnTime();
     }
 
+    //Checks for when a day has passed
     private void AlternateDaytime()
     {
-        //removes time passed from time remaining
+        //Removes time passed from time remaining
         timeRemaining -= Time.deltaTime;
-        //if there is no time remaining
+
+        //If there is no time remaining
         if (timeRemaining <= 0)
         {
-            //swaps daytime
+            //Swaps daytime
             Daytime = !Daytime;
+
             //If it is day
             if (Daytime)
             {
+                //Debug
                 DebugManager.DebugLog("Day has started");
-                //set the time remaining to the day length
+
+                //Set the time remaining to the day length
                 timeRemaining = dayLength;
             }
             //Else nightime
             else
             {
+                //Debug
                 DebugManager.DebugLog("Night has started");
-                //set the time remaining to the night length
+
+                //Set the time remaining to the night length
                 timeRemaining = nightLength;
+
                 //Send out event that Day has passed
                 EventManager.currentManager.AddEvent(new DayHasPassed());
+
+                //Mark that a day has passed
                 currentDayInTheWeek++;
+
+                //Check for a week change
                 AlternateWeek();
             }
         }
     }
-    //Blends color based on the current time of day
-    private void ChangeColorGradientBasedOnTime()
+
+    //Changes the emmision intensity of the HDRP lighting based on the expected time of day
+    private void ChangeLightingEmmisionIntensityBasedOnTime()
     {
-        //If it is day
+        //Sets the emission based on the current time of day
         if (Daytime)
         {
-            sceneLight.color=BlendColorValue(nightColor, dayColor,dayLength);
+            //Calculate change rate for the intensity based on day length
+            float changePerFrame = GetIntensityChangePerFrame(upperLuxBound - middleLuxBound, dayLength);
+
+            //If early in day
+            if (timeRemaining >= dayLength / 2)
+            {
+                //Move towards highest value
+                sceneLight.SetIntensity(Mathf.MoveTowards(sceneLight.intensity, upperLuxBound, changePerFrame));
+            }
+            //If late in day
+            else
+            {
+                //Move towards transition value
+                sceneLight.SetIntensity(Mathf.MoveTowards(sceneLight.intensity, middleLuxBound, changePerFrame));
+            }
         }
-        //Else nightime
         else
         {
-            sceneLight.color=BlendColorValue(dayColor, nightColor, nightLength);
+            //Calculate change rate for the intensity based on night length
+            float changePerFrame = GetIntensityChangePerFrame(middleLuxBound, nightLength);
+
+            //If early in night
+            if (timeRemaining >= nightLength / 2)
+            {
+                //Move towards lowest value
+                sceneLight.SetIntensity(Mathf.MoveTowards(sceneLight.intensity, lowerLuxBound, changePerFrame));
+            }
+            //If late in night
+            else
+            {
+                //Move towards transition value
+                sceneLight.SetIntensity(Mathf.MoveTowards(sceneLight.intensity, middleLuxBound, changePerFrame));
+            }
         }
     }
-    //Blends the colors together for clean transition of the color
-    private Color BlendColorValue(Color orinalColor, Color newColor,float dayOrNightLength)
+
+    //Gets the intensity based on day/night length for half a day/night cycle
+    private float GetIntensityChangePerFrame(float givenLuxBound, float dayOrNightLength)
     {
-        //convers into a fraction that is between 1-0
-        float timePassed=timeRemaining/dayOrNightLength;
-        //linear average blending
-        return (1-timePassed)*orinalColor+timePassed*newColor;
+        return givenLuxBound / (dayOrNightLength / 2) * Time.deltaTime;
     }
 
+    //Checks for when a week has passed
     private void AlternateWeek()
     {
         //If the full week has passed
