9859b52314caf1704991279e4e9e74a181f936a8
Jacques <jacques.adriaan.venter@gmail.com>
2021-06-16 11:32:55 +0200

    Language Localization Implemented

--- UI/DayWeekDisplay.cs
+++ UI/DayWeekDisplay.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Localization.Components;
 
 public class DayWeekDisplay : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;//Text for display
-    private int currentDay=1;
-    private int currentWeek = 1;
+    public int currentDay = 1;
+    public int currentWeek = 1;
 
     private void OnEnable()
     {
@@ -24,17 +25,26 @@ public class DayWeekDisplay : MonoBehaviour
     private void OnDayPassed(EventData eventData)
     {
         currentDay++;
-        UpdateTimeStamp();
+        UpdateLocalizedString();
     }
+
     private void OnWeekPassed(EventData eventData)
     {
         currentDay = 0;
         currentWeek++;
-        UpdateTimeStamp();
+        UpdateLocalizedString();
     }
 
-    private void UpdateTimeStamp()
+    private void UpdateLocalizedString()
     {
-        text.text = "Day: " + currentDay + " Week: " + currentWeek;
+        //Error handling
+        if(TryGetComponent(out LocalizeStringEvent stringEvent))
+        {
+            stringEvent.RefreshString();
+        }
+        else
+        {
+            Debug.LogWarning("No localized string event attached correctly, are you sure you set it up?");
+        }
     }
 }
