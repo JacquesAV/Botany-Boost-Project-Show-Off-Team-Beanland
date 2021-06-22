ceee8b1dae6f4811d7cfe3c591a1fa8f3270c358
Jacques <jacques.adriaan.venter@gmail.com>
2021-06-22 13:10:04 +0200

    Updated Object View Highlight
    Script now searches for all active mesh renderers in the scripts parent object and applies the color changes to them.

--- Placeables/ObjectViewHighlight.cs
+++ Placeables/ObjectViewHighlight.cs
@@ -1,48 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ObjectViewHighlight : MonoBehaviour
 {
-    private MeshRenderer meshRender = null;
-    private Color origionalMeshColor;
+    private bool hasValidRenderers = true;
+    private Dictionary<MeshRenderer, Color> meshRenders = new Dictionary<MeshRenderer, Color>();
 
     public void Start()
     {
+        //Get the mesh renderers attached to the object children
+        GetMeshRenderersInChildren();
+    }
+
+    private void GetMeshRenderersInChildren()
+    {
         //Get starting components
-        meshRender = gameObject.GetComponent<MeshRenderer>();
-        if (meshRender != null)
+        foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>().ToList())
         {
-            origionalMeshColor = meshRender.material.color;
+            //Check that a valid material and color is present, else break the entire process
+            if (renderer.material == null || renderer.material.color == null)
+            {
+                //Set to false valid renderers
+                hasValidRenderers = false;
+                break;
+            }
+            else
+            {
+                //Add to the dictionary with the original color
+                meshRenders.Add(renderer, renderer.material.color);
+            }
         }
-        else
+
+        //If the dictionary is empty or null, consider it invalid
+        if(meshRenders == null || meshRenders.Count == 0)
         {
-            Debug.LogWarning("There was no mesh renderer found on the object, please make sure one is attached to the model.");
+            //Set to false valid renderers
+            hasValidRenderers = false;
+        }
+
+        //Debug a warning about the sucess of the process
+        if (!GetHasValidRenderers())
+        {
+            return;
         }
     }
 
     public void SetRedHighlight()
     {
         //Error handling
-        if (meshRender == null) 
+        if (!GetHasValidRenderers())
         {
-            Debug.LogWarning("There was no mesh renderer found on the object, please make sure one is attached to the model.");
             return;
         }
 
-        //Flash the object red until the timer runs out
-        meshRender.material.color = Color.red;
+        //Set the object colors to red
+        foreach (KeyValuePair<MeshRenderer, Color> renderer in meshRenders)
+        {
+            renderer.Key.material.color = Color.red;
+        }
     }
 
     public void ResetHighlight()
     {
         //Error handling
-        if (meshRender == null) 
+        if (!GetHasValidRenderers())
         {
-            Debug.LogWarning("There was no mesh renderer found on the object, please make sure one is attached to the model.");
             return;
         }
-        //Reset back to the normal coloration
-        meshRender.material.color = origionalMeshColor;
+
+        //Reset the object colors
+        foreach (KeyValuePair<MeshRenderer, Color> renderer in meshRenders)
+        {
+            renderer.Key.material.color = renderer.Value;
+        }
+    }
+
+    private bool GetHasValidRenderers()
+    {
+        //Check if valid
+        if (!hasValidRenderers)
+        {
+            //Debug
+            Debug.LogWarning("There were no mesh renderers found in the object, please make sure one is attached to the models.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
