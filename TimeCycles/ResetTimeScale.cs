606173a214904276f801ad2ec353c63482c78fb6
Jacques <jacques.adriaan.venter@gmail.com>
2021-06-23 09:34:15 +0200

    Implemented ResetTimeScale

---
+++ TimeCycles/ResetTimeScale.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTimeScale : MonoBehaviour
+{
+    public void ResetScale()
+    {
+        Time.timeScale = 1;
+    }
+}
